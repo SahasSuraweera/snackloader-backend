94c13324b8df2f2e060b63c1065bc4e28a21b5c2
Sahas Suraweera <sahassuraweera11@gmail.com>
2025-11-18 18:36:17 +0530

    Updated app.js file

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ const cors = require("cors");
 require("dotenv").config();
 
 const deviceRoutes = require("./routes/deviceRoutes");
+const testRoutes = require("./routes/testRoutes")
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/device", deviceRoutes);
+
+app.use("/test", testRoutes);
 
 app.get("/", (req, res) => {
   res.send("SnackLoader Backend Running");
